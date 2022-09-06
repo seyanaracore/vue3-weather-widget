dd3ce503ede92b6b71b14b1750c887db0e9277b0
Anton Tigunov <seyanaracore@gmail.com>
2022-09-06 16:19:28 +0300

    feat: config project

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,12 +7,68 @@ module.exports = {
     'plugin:vue/vue3-essential',
     '@vue/airbnb',
     '@vue/typescript/recommended',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 'latest',
   },
+  parser: 'vue-eslint-parser',
   rules: {
+    'no-restricted-exports': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: 'import',
+        next: '*',
+      },
+      {
+        blankLine: 'any',
+        prev: 'import',
+        next: 'import',
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'block-like',
+      },
+      {
+        blankLine: 'always',
+        prev: 'block-like',
+        next: '*',
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: ['const', 'let'],
+      },
+      {
+        blankLine: 'always',
+        prev: ['const', 'let'],
+        next: '*',
+      },
+      {
+        blankLine: 'never',
+        prev: ['singleline-const', 'singleline-let'],
+        next: ['singleline-const', 'singleline-let'],
+      },
+      {
+        blankLine: 'always',
+        prev: ['multiline-const', 'multiline-let'],
+        next: ['multiline-const', 'multiline-let'],
+      },
+      {
+        blankLine: 'always',
+        prev: ['cjs-import'],
+        next: '*',
+      },
+      {
+        blankLine: 'any',
+        prev: ['cjs-import'],
+        next: ['cjs-import'],
+      },
+    ],
   },
-};
+}
