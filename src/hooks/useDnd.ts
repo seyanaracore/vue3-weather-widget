c7495a88e80fb31f16d042bee06ec35268281a3d
Anton Tigunov <seyanaracore@gmail.com>
2022-10-09 23:20:07 +0300

    feat: drag and drop hook

---
+++ src/hooks/useDnd.ts
@@ -0,0 +1,49 @@
+type DragEl = HTMLElement
+type DragInitiatorEl = HTMLElement | null
+
+/**
+ * Drag and drop hook
+ * @param {dragTarget} dragTarget - html element which will move
+ * @param {dragInitiator} dragInitiator - html element which will initiate the move
+ */
+
+const useDnd = (dragTarget: DragEl, dragInitiator: DragInitiatorEl = null) => {
+  const dragEl = dragTarget
+
+  const isDraggable = {
+    isDraggable: false,
+    get() {
+      return this.isDraggable
+    },
+    set(val: boolean) {
+      this.isDraggable = val
+      dragEl.draggable = val
+    },
+  }
+
+  const onDropEnd = () => {
+    isDraggable.set(false)
+  }
+
+  const initDown = () => {
+    isDraggable.set(true)
+
+    const mouseDownEnv = new Event('mousedown')
+
+    dragEl.dispatchEvent(mouseDownEnv)
+  }
+
+  isDraggable.set(!dragInitiator && dragInitiator !== dragTarget) // if dragInitiator is null just set true
+
+  dragEl.ondragenter = (e) => e.preventDefault()
+  dragEl.ondragover = (e) => e.preventDefault()
+
+  if (dragInitiator && dragInitiator !== dragTarget) {
+    const dragInitiatorEl = dragInitiator
+
+    dragInitiatorEl.onmouseup = onDropEnd
+    dragInitiatorEl.onmousedown = initDown
+  }
+}
+
+export default useDnd
