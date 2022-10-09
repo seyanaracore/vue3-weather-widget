6cabc2b5ab6933b20eb65bc361aab6427075e124
Anton Tigunov <seyanaracore@gmail.com>
2022-10-09 23:21:55 +0300

    feat: no cities component

---
+++ src/components/NoCities.vue
@@ -0,0 +1,9 @@
+<template>
+  <Error class="my-5 text-center" type="secondary">{{ noCityMessage }}</Error>
+</template>
+
+<script setup lang="ts">
+import Error from './UI/ErrorUi.vue'
+
+const noCityMessage = 'The list of cities is empty.'
+</script>
