da917fdf0eddea251b911558af282587467d2b15
Anton Tigunov <seyanaracore@gmail.com>
2022-10-09 23:16:59 +0300

    feat: UI components
    Error, Loader, Header

---
+++ src/components/UI/LoaderUI.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="spinner-border" :class="`text-${type}`" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { withDefaults } from 'vue'
+
+interface IProps {
+  type?: string
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const props = withDefaults(defineProps<IProps>(), {
+  type: 'primary',
+})
+</script>
