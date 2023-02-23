cb7a7bb67f697e6f0366a477161a4879eaff0840
Anton Tigunov <seyanaracore@gmail.com>
2023-02-23 16:28:09 +0300

    feat: local storage util

---
+++ src/utils/localStorageUtil.ts
@@ -0,0 +1,35 @@
+class LocalStorageUtil {
+  static get(key: string): unknown {
+    if (!key) return null
+
+    try {
+      const lcItem = localStorage.getItem(key)
+
+      if (!lcItem) return null
+      return JSON.parse(lcItem)
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error)
+    }
+
+    return null
+  }
+
+  static set(value: unknown, key: string) {
+    if (!key) return
+
+    try {
+      localStorage.setItem(key, JSON.stringify(value))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  static delete(key: string) {
+    if (!key) return
+
+    localStorage.removeItem(key)
+  }
+}
+
+export default LocalStorageUtil
